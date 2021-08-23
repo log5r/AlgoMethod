23e53a06c90b1cd9b192f8cf9f36dca4caa4e80c
log5 <40707031+judau@users.noreply.github.com>
2021-08-23 23:53:57 +0900

    remove unnecessary semicolons

--- algo-method/324/algo-324.swift
+++ algo-method/324/algo-324.swift
@@ -5,11 +5,11 @@ grid[0] = cinAsIntList()
 
 for h in 1...3 {
     for w in 0...3 where w >= h {
-        var buf: Int = 0;
+        var buf: Int = 0
         for k in -1...1 where w - k >= 0 && w + k <= 3 {
             buf += grid[h - 1][w + k]
         }
-        grid[h][w] = buf;
+        grid[h][w] = buf
     }
 }
 print(grid[3][3])
