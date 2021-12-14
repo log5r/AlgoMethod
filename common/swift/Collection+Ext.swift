7a8f87fdff7cc70201197082cab6657fafefdfa6
log5 <40707031+judau@users.noreply.github.com>
2021-12-14 21:12:25 +0900

    add ext

---
+++ common/swift/Collection+Ext.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Collection where Element: BinaryFloatingPoint {
+    var average: Double { isEmpty ? .zero : Double(reduce(.zero, +)) / Double(count) }
+    var variance: Double {
+        let avr = self.average
+        return self.reduce(.zero, { $0 + pow(Double($1) - avr, 2) }) / Double(count)
+    }
+    var sigma: Double { sqrt(variance) }
+}
