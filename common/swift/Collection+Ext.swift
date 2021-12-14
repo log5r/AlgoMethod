95d7914f979ffd03c3a20478575d0c5f907c0464
log5 <40707031+judau@users.noreply.github.com>
2021-12-14 21:13:51 +0900

    add comment

--- common/swift/Collection+Ext.swift
+++ common/swift/Collection+Ext.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Collection where Element: BinaryFloatingPoint {
-    var average: Double { isEmpty ? .zero : Double(reduce(.zero, +)) / Double(count) }
+    var average: Double { isEmpty ? .zero : Double(reduce(.zero, +)) / Double(count) } // :thinking_face:
     var variance: Double {
         let avr = self.average
         return self.reduce(.zero, { $0 + pow(Double($1) - avr, 2) }) / Double(count)
