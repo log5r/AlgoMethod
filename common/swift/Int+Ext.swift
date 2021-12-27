57d481ee9c0780b56ca333070725622d6af80cb8
log5 <40707031+judau@users.noreply.github.com>
2021-12-27 23:18:56 +0900

    update ext

--- common/swift/Int+Ext.swift
+++ common/swift/Int+Ext.swift
@@ -1,7 +1,21 @@
 import Foundation
 
 extension Int {
-    func sqrtFloor() -> Int {
-        Int(Double(self).squareRoot())
+
+    var sqrt: Int { Int(Double(self).squareRoot()) }
+
+    func divisors() -> [Int] {
+        precondition(self >= 1)
+        var result = [Int]()
+        var i = 1
+        while i * i <= self {
+            if self % i == 0 {
+                result.append(i)
+                if i * i != self { result.append(self / i) }
+            }
+            i += 1
+        }
+        return result.sorted()
     }
+
 }
